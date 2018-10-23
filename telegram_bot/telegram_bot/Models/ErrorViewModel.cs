300a9f77790989185e8dbb8944f253c38f02cb2b
Rishat Latfullin <Marearcat@outlook.com>
2018-10-23 16:18:40 +0300

    + init project

---
+++ telegram_bot/telegram_bot/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace telegram_bot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
